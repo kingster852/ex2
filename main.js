dddbcd101bf1f3f0f37266b107452b528b387103
kingster <57381795+kingster852@users.noreply.github.com>
2023-09-16 23:16:21 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+  const app = Vue.createApp({
+    data() {
+      return {
+        message: "Hello World!",
+        vueClass: "pinkBG",
+        url1: "https://github.com/kingster852/ex1/raw/main/"
+      }
+    }
+  })
+  app.mount('#app')
